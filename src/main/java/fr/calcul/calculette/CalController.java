acb26ba8967bd66b5204f4bd129ace49d16a8055
Gispsy <mordredbta3011@gmail.com>
2023-01-06 12:24:08 +0100

    CalculetteModif1

--- src/main/java/fr/calcul/calculette/CalController.java
+++ src/main/java/fr/calcul/calculette/CalController.java
@@ -26,55 +26,62 @@ public class CalController {
 
 //Fonction pour l'affichage dans la calculatrice
     public void ButtonClick0(ActionEvent actionEvent) {     //Button 0
-
-        //TextField.setText(Button0);
         TextField.appendText("0");
+        int B0 = 0;
 
     }
 
     public void ButtonClick9(ActionEvent actionEvent) {     //Button 9
-
         TextField.appendText("9");
-        //TextField.setText(Button9);
+        int B9 = 9;
+
     }
 
     public void ButtonClick8(ActionEvent actionEvent) {     //Button 8
         TextField.appendText("8");
+        int B8 = 8;
 
     }
 
     public void ButtonClick7(ActionEvent actionEvent) {     //Button 7
         TextField.appendText("7");
+        int B7 = 7;
 
     }
 
     public void ButtonClick6(ActionEvent actionEvent) {     //Button 6
         TextField.appendText("6");
+        int B6 = 6;
 
     }
 
     public void ButtonClick5(ActionEvent actionEvent) {     //Button 5
         TextField.appendText("5");
+        int B5 = 5;
 
     }
 
     public void ButtonClick4(ActionEvent actionEvent) {     //Button 4
         TextField.appendText("4");
+        int B4 = 4;
 
     }
 
     public void ButtonClick3(ActionEvent actionEvent) {     //Button 3
         TextField.appendText("3");
+        int B3 = 3;
 
     }
 
     public void ButtonClick2(ActionEvent actionEvent) {     //Button 2
         TextField.appendText("2");
+        int B2 = 2;
 
     }
 
     public void ButtonClick1(ActionEvent actionEvent) {     //Button 1
         TextField.appendText("1");
+        int B1 = 1;
 
     }
 
@@ -103,15 +110,15 @@ public class CalController {
 
     }
 
-    public void ButtonClickPoint(ActionEvent actionEvent) {
+    public void ButtonClickPoint(ActionEvent actionEvent) {     //La virgule
         TextField.appendText(".");
 
     }
 
     public void ButtonClickEgale(ActionEvent actionEvent) {     //Fait le calcule
-        String Text = TextField.getText();
-
-        int resultat = Integer.parseInt(Text);
+        for (int i = 0; i < TextField.Length(); i++){
+            
+        }
 
     }
 }
