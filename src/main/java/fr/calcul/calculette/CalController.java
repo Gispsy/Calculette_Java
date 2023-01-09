0d00676af6c2c3cbe5b17430834b8003cc0cd62f
Gispsy <mordredbta3011@gmail.com>
2023-01-09 10:54:00 +0100

    A finir voir AdditionneurProject

--- src/main/java/fr/calcul/calculette/CalController.java
+++ src/main/java/fr/calcul/calculette/CalController.java
@@ -24,84 +24,91 @@ public class CalController {
     public Button ButtonMultiplication;
     public Button ButtonDivision;
 
+    private String operateur = "";
+    private double chiffre;
+
 //Fonction pour l'affichage dans la calculatrice
     public void ButtonClick0(ActionEvent actionEvent) {     //Button 0
         TextField.appendText("0");
-        int B0 = 0;
+        chiffre = 0;
 
     }
 
     public void ButtonClick9(ActionEvent actionEvent) {     //Button 9
         TextField.appendText("9");
-        int B9 = 9;
+        chiffre = 9;
 
     }
 
     public void ButtonClick8(ActionEvent actionEvent) {     //Button 8
         TextField.appendText("8");
-        int B8 = 8;
+        chiffre = 8;
 
     }
 
     public void ButtonClick7(ActionEvent actionEvent) {     //Button 7
         TextField.appendText("7");
-        int B7 = 7;
+        chiffre = 7;
 
     }
 
     public void ButtonClick6(ActionEvent actionEvent) {     //Button 6
         TextField.appendText("6");
-        int B6 = 6;
+        chiffre = 6;
 
     }
 
     public void ButtonClick5(ActionEvent actionEvent) {     //Button 5
         TextField.appendText("5");
-        int B5 = 5;
+        chiffre = 5;
 
     }
 
     public void ButtonClick4(ActionEvent actionEvent) {     //Button 4
         TextField.appendText("4");
-        int B4 = 4;
+        chiffre = 4;
 
     }
 
     public void ButtonClick3(ActionEvent actionEvent) {     //Button 3
         TextField.appendText("3");
-        int B3 = 3;
+        chiffre = 3;
 
     }
 
     public void ButtonClick2(ActionEvent actionEvent) {     //Button 2
         TextField.appendText("2");
-        int B2 = 2;
+        chiffre = 2;
 
     }
 
     public void ButtonClick1(ActionEvent actionEvent) {     //Button 1
         TextField.appendText("1");
-        int B1 = 1;
+        chiffre = 1;
 
     }
 
     public void ButtonClickAdd(ActionEvent actionEvent) {       //Addition
         TextField.appendText("+");
+        operateur = "+";
 
     }
 
     public void ButtonClickSous(ActionEvent actionEvent) {      //Soustraction
         TextField.appendText("-");
+        operateur = "-";
 
     }
 
     public void ButtonClickMulti(ActionEvent actionEvent) {     //Multiplication
         TextField.appendText("*");
+        operateur = "*";
 
     }
 
     public void ButtonClickDivi(ActionEvent actionEvent) {      //Division
         TextField.appendText("/");
+        operateur = "/";
 
     }
 
@@ -116,9 +123,25 @@ public class CalController {
     }
 
     public void ButtonClickEgale(ActionEvent actionEvent) {     //Fait le calcule
-        for (int i = 0; i < TextField.Length(); i++){
-            
-        }
+        Calcul();
+    }
 
+    private void Calcul(){
+        if(operateur.equals("+")){
+            chiffre +
+
+        }
+        if(operateur.equals("-")){
+
+
+        }
+        if(operateur.equals("*")){
+
+
+        }
+        if(operateur.equals("/")){
+
+
+        }
     }
 }
